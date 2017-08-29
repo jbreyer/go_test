764c3b43a44c456ad9fe6155c668483d2ca5a673
jbreyer <3ntr0py@outlook.com>
2017-08-29 19:40:20 -0400

    a simple echo

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
-	fmt.Println("Hello World!")
+	var s, sep string
+
+	for i:=1; i<len(os.Args); i++{
+		s += sep + os.Args[i]
+		sep = " "
+	}
+
+	fmt.Println(s)
 }
